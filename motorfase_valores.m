beb4181f339a144cf6323998619a23084c2dd548
RicardoTakahashi <ricardotakahashi@usp.br>
2022-09-14 10:49:25 -0300

    modelos do simulink para simulação de acionamento e controle de motores bldcm p1/2: 1 ou 2 malhas de controle sem ou com anti: anti-windup
    motorfase_valores.m contém os valores da máquina a ser estudada

---
+++ motorfase_valores.m
@@ -0,0 +1,8 @@
+R = 2.3;%Resistência dos enrolamentos
+p = 3; %Número de pares de polos
+flux = 0.12; %Fluxo rotórico
+inddiff = 0.0125; %Valor de L-M
+J = 0.0042;%Momento de Inércia
+B = 0.003032;
+load = 0.1; %Torque equivalente a carga que recebe a potência motriz
+kt = 0.2;
